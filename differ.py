3c60673c927df4156b4d505ea770357e86c57210
Rohith K <rohith.k678@gmail.com>
2019-09-20 09:58:36 +0530

    difference function

--- differ.py
+++ differ.py
@@ -1,4 +1,4 @@
-a = input("enter a number: ")
-b = input("enter another number: ")
+a = input("enter a number:  ")
+b = input("enter another number:  ")
 diff = int(a) - int(b)
 print("difference is {}".format(diff))
